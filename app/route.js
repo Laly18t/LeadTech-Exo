e54399fe581895ae792a0fb5d866169ae28daa6c
Laly <laly.teissier@gmail.com>
2025-10-07 23:17:41 +0200

    feat: add token bucket algo

--- app/route.js
+++ app/route.js
@@ -1,8 +1,44 @@
 const formValidator = require('./form_validator');
 const photoModel = require('./photo_model');
 const { publishZipRequest } = require('./pubsub_service.js');
-const { pubsubClient, getAllJobs, getJobStatus } = require('./pubsub_client.js');
+const pubsubClient = require('./pubsub_client.js');
 const moment = require('moment');
+
+const rateLimiterByIp = new Map();
+
+function checkRateLimit(req) {
+  var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || null;
+
+  const now = Date.now();
+  const rateLimitConfig = {
+    r: 1, // tokens per second
+    b: 15, // bucket size
+    cost: 3 // cost per request
+  };
+
+  if (!rateLimiterByIp.has(ip)) { // première requête de cet IP
+    rateLimiterByIp.set(ip, {
+      tokens: rateLimitConfig.b,
+      lastRefill: now
+    });
+  }
+
+  const userData = rateLimiterByIp.get(ip); // on récupère les données de l'utilisateur
+  const timeElapsed = (now - userData.lastRefill) / 1000; // temps écoulé en secondes
+  const tokensToAdd = timeElapsed * rateLimitConfig.r; // jetons à ajouter
+
+  userData.tokens = Math.min(rateLimitConfig.b, userData.tokens + tokensToAdd); // on ajoute les jetons
+  userData.lastRefill = now; // on met à jour le timestamp
+
+  console.log(`Rate limit check for IP: ${ip} - Tokens available: ${userData.tokens.toFixed(2)}`);
+
+  if (userData.tokens >= rateLimitConfig.cost) { // assez de jetons
+    userData.tokens -= rateLimitConfig.cost; // on consomme les jetons
+    return true; // la requête est autorisée
+  } else {
+    return false; // limite de taux dépassée
+  }
+}
 
 function route(app) {
   app.get('/', (req, res) => {
@@ -44,6 +80,14 @@ function route(app) {
 
   // Recup des tags depuis les query params
   app.post('/zip', async (req, res) => {
+
+    if (!checkRateLimit(req)) {
+      return res.status(429).json({
+        success: false,
+        error: 'Trop de requêtes. Veuillez réessayer plus tard.'
+      });
+    }
+
     try {
       const tags = req.query.tags; // tag recup depuis le query
 
@@ -62,14 +106,14 @@ function route(app) {
       // publication dans la queue
       const messageId = await publishZipRequest(tags);
 
-      res.json({
+      return res.json({
         success: true,
         message: 'Demande de zip envoyée',
         tags: tags,
         messageId: messageId,
         queueTopic: `dmii-${process.env.PUBSUB_TOPIC_NUMBER}`
       });
-      return res.render('index', ejsLocalVariables);
+      // return res.render('index', ejsLocalVariables);
 
     } catch (error) {
       console.error('Erreur dans l\'endpoint /zip:', error);
